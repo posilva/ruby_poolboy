48b379c7c9c710833a4216be2678dcb405d65b7d
Pedro Silva <Pedro.Silva@Miniclip.com>
2019-07-12 14:41:15 +0100

    first commit

---
+++ lib/ruby_poolboy.rb
@@ -0,0 +1,6 @@
+require "ruby_poolboy/version"
+
+module RubyPoolboy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ruby_poolboy/version.rb
@@ -0,0 +1,3 @@
+module RubyPoolboy
+  VERSION = "0.1.0"
+end
